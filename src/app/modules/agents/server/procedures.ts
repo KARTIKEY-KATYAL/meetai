b6c4a67d2442d0fef10be19946f494fa318fc708
Kartikey Katyal <kartikeykatyal2003@gmail.com>
2025-05-31 14:36:11 +0530

    feat: implement agent detail view with loading and error states, enhance agent retrieval logic, and add agent header component

--- src/app/modules/agents/server/procedures.ts
+++ src/app/modules/agents/server/procedures.ts
@@ -9,14 +9,22 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 
 export const agentsRouter = createTRPCRouter({
     // Change getMany to use 'ProtectedProcedure'
-    getOne: protectedProcedure.input(z.object({ id: string() })).query(async ({ input }) => {
+    getOne: protectedProcedure.input(z.object({ id: string() }))
+    .query(async ({ input , ctx }) => {
         const [existingAgent] = await db
             .select({
                 meetingCount: sql<number>`5`,
                 ...getTableColumns(agents),
             })
             .from(agents)
-            .where(eq(agents.id, input.id))
+            .where(and(
+                eq(agents.id, input.id),
+                eq(agents.userId,ctx.auth.user.id)
+            ))
+
+            if (!existingAgent){
+                throw new TRPCError({code:"NOT_FOUND",message:"Agent not Found"})
+            }
         // throw new TRPCError({"code" : "BAD_REQUEST"})
 
         return existingAgent;
---
+++ src/app/modules/agents/ui/components/agent-id-view.tsx
@@ -0,0 +1,80 @@
+interface Props {
+  agentid: string;
+  agentName: string;
+  onEdit: () => {};
+  onRemove: () => {};
+}
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  ChevronRight,
+  ChevronRightIcon,
+  MoreVerticalIcon,
+  PencilIcon,
+  TrashIcon,
+} from "lucide-react";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export const AgentIdViewHeader = ({
+  agentid,
+  agentName,
+  onEdit,
+  onRemove,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link href="/agents">My Agents</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+            <ChevronRightIcon />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              asChild
+              className="font-medium text-xl text-foreground"
+            >
+              <Link href={`/agents/${agentid}`}>{agentName}</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className="size-4 text-white" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={onRemove}>
+            <TrashIcon className="size-4 text-white" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
